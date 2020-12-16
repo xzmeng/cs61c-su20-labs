aab21ebca2530a3ebfa5dc4cfb1449e8676cba50
Meng Xiangzhuo <aumo@foxmail.com>
2020-12-16 17:53:32 +0800

    Finish lab03.

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -21,38 +21,37 @@ dest:
 
 .text
 main:
-    addi t0, x0, 0
-    addi s0, x0, 0
-    la s1, source
+    addi t0, x0, 0 # int k;
+    addi s0, x0, 0 # int sum = 0;
+    la s1, source  
     la s2, dest
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
-    sw t0, 0(sp)
-    sw t2, 4(sp)
-    jal square
-    lw t0, 0(sp)
+    slli s3, t0, 2 # s3 = k *=4
+    add t1, s1, s3 # t1 = &source[k]
+    lw t2, 0(t1)   # t2 = source[k]
+    beq t2, x0, exit # if (source[k] == 0) jump to exit
+    add a0, x0, t2   # a0 = source[k]
+    addi sp, sp, -8  # 
+    sw t0, 0(sp)     # save register
+    sw t2, 4(sp)     jal square
+    lw t0, 0(sp)     # recover register
     lw t2, 4(sp)
     addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
+    add t2, x0, a0 # t2 = result
+    add t3, s2, s3 # t3 = &dest[k]
+    sw t2, 0(t3)   # dest[k] = result
+    add s0, s0, t2 # sum = sum + dest[k]
+    addi t0, t0, 1 # k++
     jal x0, loop
 square:
-    add t0, a0, x0
-    add t1, a0, x0
-    addi t0, t0, 1
-    addi t2, x0, -1
-    mul t1, t1, t2
-    mul a0, t0, t1
+    add t0, a0, x0 # t0 = x
+    add t1, a0, x0 # t1 = x
+    addi t0, t0, 1 # t0 = x + 1
+    addi t2, x0, -1 # t2 = -1
+    mul t1, t1, t2 # t1 = -x
+    mul a0, t0, t1 # a0 = (x + 1) * (-x)
     jr ra
 exit:
-    add a0, x0, s0
+    add a0, x0, s0 # a0 = sum
     add a1, x0, x0
     ecall # Terminate ecall
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -5,11 +5,11 @@ n: .word 8
 
 .text
 main:
-    la t0, n
-    lw a0, 0(t0)
-    jal ra, factorial
+    la t0, n     # t0 = &n
+    lw a0, 0(t0) # a0 = 8
+    jal ra, factorial  # save ra on stack , goto factorial 
 
-    addi a1, a0, 0
+    addi a1, a0, 0  
     addi a0, x0, 1
     ecall # Print Result
 
@@ -22,3 +22,14 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    add t0, a0, x0 # t0 = n
+    addi a0, x0, 1 # a0 = 1
+loop:
+    bge x0, t0, ret
+    mul a0, a0, t0
+    addi t0, t0, -1
+    jal x0, loop
+
+ret:
+    jr ra
+    
